0a1e1d5122be5ab1d152cb1199e7058050ee3b33
Rockwell Schrock <rockwell@schrock.me>
2025-08-18 16:48:24 -0400

    Looser reqs for loading i18n dependencies

--- lib/trmnl/liquid.rb
+++ lib/trmnl/liquid.rb
@@ -10,8 +10,11 @@ rescue LoadError
   nil
 end
 
-if defined?(::I18n) && defined?(::TRMNL::I18n)
+if defined?(::TRMNL::I18n)
   ::TRMNL::I18n.load_locales
+end
+
+if Gem.loaded_specs['rails-i18n']
   ::I18n.load_path += Dir[File.join(Gem.loaded_specs['rails-i18n'].full_gem_path, 'rails', 'locale', '*.yml')]
 end
 
